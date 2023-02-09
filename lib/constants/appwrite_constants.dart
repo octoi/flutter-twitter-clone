c984ebf33c1545b3a01c42ecdaf341870ed4ba39
Fadhil <fadhilsaheer@gmail.com>
2023-02-09 19:44:24 +0530

    taking values from environment variables

--- lib/constants/appwrite_constants.dart
+++ lib/constants/appwrite_constants.dart
@@ -1,13 +1,20 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 class AppwriteConstants {
-  static const String databaseId = '63ca9fd892b6a9f5e135';
-  static const String projectId = '63ca9d4f81ce9074804f';
-  static const String endPoint = 'http://YOUR_IP/v1';
+  static String databaseId =
+      dotenv.env['DATABASE_ID'] ?? '63ca9fd892b6a9f5e135';
+  static String projectId = dotenv.env['PROJECT_ID'] ?? '63ca9d4f81ce9074804f';
+  static String endPoint = dotenv.env['ENDPOINT'] ?? 'http://YOUR_IP/v1';
 
-  static const String usersCollection = '63cb8ae73960973b0635';
-  static const String tweetsCollection = '63cbd6781a8ce89dcb95';
-  static const String notificationsCollection = '63cd5ff88b08e40a11bc';
+  static String usersCollection =
+      dotenv.env['USERS_COLLECTION'] ?? '63cb8ae73960973b0635';
+  static String tweetsCollection =
+      dotenv.env['TWEETS_COLLECTION'] ?? '63cbd6781a8ce89dcb95';
+  static String notificationsCollection =
+      dotenv.env['NOTIFICATIONS_COLLECTION'] ?? '63cd5ff88b08e40a11bc';
 
-  static const String imagesBucket = '63cbdab48cdbccb6b34e';
+  static String imagesBucket =
+      dotenv.env['IMAGE_BUCKET'] ?? '63cbdab48cdbccb6b34e';
 
   static String imageUrl(String imageId) =>
       '$endPoint/storage/buckets/$imagesBucket/files/$imageId/view?project=$projectId&mode=admin';
